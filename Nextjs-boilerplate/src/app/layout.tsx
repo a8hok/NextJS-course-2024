c81266d9db42273e8e2978dad5216038471746f2
a8hok <ashokkumar.explore@gmail.com>
2024-05-13 05:15:34 +0530

    test precommit hook

--- Nextjs-boilerplate/src/app/layout.tsx
+++ Nextjs-boilerplate/src/app/layout.tsx
@@ -3,6 +3,8 @@ import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 
+const a = 10;
+
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
