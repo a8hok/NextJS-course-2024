3e408100da55b00d56a602e324703b788376331a
a8hok <ashokkumar.explore@gmail.com>
2024-05-13 05:48:56 +0530

    updated config

---
+++ Nextjs-boilerplate/.lintstagedrc
@@ -0,0 +1,4 @@
+module.exports = {
+  "*.{js,jsx,ts,tsx}": ["prettier --write", "eslint --fix"],
+  "*.{json,css,md}": ["prettier --write"]
+};
--- Nextjs-boilerplate/.lintstagedrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  // Run type-check on changes to TypeScript files
-  '**/*.ts?(x)': () => 'yarn type-check',
-  // Lint & Prettify TS and JS files
-  '**/*.(ts|tsx|js)': (filenames) => [
-    `npm run lint:fix . ${filenames.join(' ')}`,
-    `npm run prettier --write ${filenames.join(' ')}`,
-  ],
-};
